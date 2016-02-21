5b974426c379e61e0c46f29ed044f6c6c967d5ac
Yu Lu <yulu8798@gmail.com>
2016-02-22 00:11:13 +0800

    update demo app ui

--- app/src/main/java/me/littlecheesecake/cropimagelayout/MainActivity.java
+++ app/src/main/java/me/littlecheesecake/cropimagelayout/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import me.littlecheesecake.croplayout.EditPhotoView;
 import me.littlecheesecake.croplayout.EditableImage;
@@ -18,14 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final EditPhotoView imageView = (EditPhotoView) findViewById(R.id.editable_image);
+        final TextView boxText = (TextView) findViewById(R.id.box_text);
         final EditableImage image = new EditableImage(this, R.drawable.photo2);
-        ScalableBox box = new ScalableBox(20, 20, 150,150);
+        ScalableBox box = new ScalableBox(25,180,640,880);
         image.setBox(box);
         imageView.initView(this, image);
+
+        boxText.setText("box: [" + 25 + "," + 180 +"],[" + 640 + "," + 880 + "]");
         imageView.setOnBoxChangedListener(new OnBoxChangedListener() {
             @Override
             public void onChanged(int x1, int y1, int x2, int y2) {
-                System.out.println("box: [" + x1 + "," + y1 +"],[" + x2 + "," + y2 + "]");
+                boxText.setText("box: [" + x1 + "," + y1 +"],[" + x2 + "," + y2 + "]");
             }
         });
 
